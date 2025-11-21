f72877f4181a1c7b6183e2fb9343d9c220edca15
weichaozeng <zvc22@outlook.com>
2025-11-22 00:22:30 +0800

    Update pa_nms

--- ultralytics/custom/nms.py
+++ ultralytics/custom/nms.py
@@ -9,8 +9,8 @@ from ultralytics.utils import LOGGER
 from ultralytics.utils.metrics import batch_probiou, box_iou
 from ultralytics.utils.ops import xywh2xyxy
 
-point_thres = 0.95
-bone_thres = 0.95
+point_thres = 0.99
+bone_thres = 0.99
 def pose_aware_non_max_suppression(
     prediction,
     conf_thres: float = 0.25,
