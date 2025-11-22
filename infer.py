3b055681acdff24d1083bafd48e87f1da1f7c163
weichaozeng <zvc22@outlook.com>
2025-11-22 12:30:10 +0800

    Update infer

--- infer.py
+++ infer.py
@@ -134,9 +134,9 @@ def draw_bbox(img_cv2, id, box, is_right):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Inference")
-    parser.add_argument("--in_dir", type=str, default="example_data/FPHA/")
-    parser.add_argument("--in_type", type=str, choices=["video", "rgb", "color", ""], default="color")
-    parser.add_argument("--save_dir", type=str, default="example_out/wilor/FPHA/")
+    parser.add_argument("--in_dir", type=str, default="example_data/images/")
+    parser.add_argument("--in_type", type=str, choices=["video", "rgb", "color", ""], default="")
+    parser.add_argument("--save_dir", type=str, default="example_out/pa_nms/images/")
     parser.add_argument("--save_type", type=str, default="img")
     parser.add_argument("--det_thresh", type=float, default=0.5)
     parser.add_argument("--ckpt", type=str, default="weights/detector.pt")
