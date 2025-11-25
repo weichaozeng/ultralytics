f0dd0aab5f91cc53c4f20c87ed0267fb196adda2
weichaozeng <zvc22@outlook.com>
2025-11-25 15:59:56 +0800

    Update kalman_filter_pose

--- ultralytics/custom/kalman_filter_pose.py
+++ ultralytics/custom/kalman_filter_pose.py
@@ -4,4 +4,11 @@ import scipy.linalg
 from ultralytics.trackers.utils.kalman_filter import KalmanFilterXYAH
 
 
-class KalmanFilterPose(KalmanFilterXYAH):
+class KalmanFilterPose:
+    def __init__(self):
+        ndim = 42 
+        dt = 1.0
+        # F = [[I, dt*I], [0, I]]
+        self._motion_mat = np.eye(2 * ndim, 2 * ndim)
+        for i in range(ndim):
+            self._motion_mat[i, ndim + i] = dt
