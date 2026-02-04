837f6b9dc6499a3aabdc381ed766085bd4e9dc15
weichaozeng <zvc22@outlook.com>
2026-02-04 22:28:14 +0800

    update

--- infer.py
+++ infer.py
@@ -223,7 +223,7 @@ if __name__ == "__main__":
     parser.add_argument("--in_type", type=str, choices=["video", "rgb", "color", ""], default="video")
     parser.add_argument("--save_dir", type=str, default="example_out/pa_nms_posetrack/videos/")
     parser.add_argument("--save_type", type=str, default="video")
-    parser.add_argument("--det_thresh", type=float, default=0.2)
+    parser.add_argument("--det_thresh", type=float, default=0.3)
     parser.add_argument("--ckpt", type=str, default="weights/detector.pt")
     parser.add_argument("--tracker", type=str, default="posetrack")
 
