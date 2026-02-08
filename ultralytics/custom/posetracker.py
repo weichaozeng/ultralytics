260b8101a8122201481eeebb8b2c1da84c352e2a
weichaozeng <zvc22@outlook.com>
2026-02-08 17:01:52 +0800

    update

--- ultralytics/custom/posetracker.py
+++ ultralytics/custom/posetracker.py
@@ -217,9 +217,9 @@ class PoseTracker(BOTSORT):
         super().__init__(args, frame_rate)
         self.pose_kalman_filter = KalmanFilterPose()
         self.box_gate_thresh = getattr(args, 'box_gate_thresh', 9.488) 
-        self.first_match_thresh = getattr(args, 'first_match_thresh', 0.5)
-        self.second_match_thresh = getattr(args, 'second_match_thresh', 0.3)
-        self.unconf_match_thresh = getattr(args, 'unconf_match_thresh', 0.3)
+        self.first_match_thresh = getattr(args, 'first_match_thresh', 0.8)
+        self.second_match_thresh = getattr(args, 'second_match_thresh', 0.5)
+        self.unconf_match_thresh = getattr(args, 'unconf_match_thresh', 0.5)
 
         # for interacting in get_dists
         self.W_POSE = 0.6
