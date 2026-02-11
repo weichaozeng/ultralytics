24afd2a2657fdbe148f53970dd7eb031c13bd3d6
weichaozeng <zvc22@outlook.com>
2026-02-11 18:49:35 +0800

    update

--- ultralytics/custom/posetracker.py
+++ ultralytics/custom/posetracker.py
@@ -265,7 +265,7 @@ class PoseTracker(BOTSORT):
         self.frame_id += 1
         print(self.frame_id)
         print(f"dets: {len(dets)}")
-        if self.frame_id in [345, 346]:
+        if self.frame_id in [423, 424]:
             print("here")
     
         activated_stracks = []  
