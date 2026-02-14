11731059763d1205587ea2501884fc4920fed7f3
weichaozeng <zvc22@outlook.com>
2026-02-14 10:44:58 +0800

    update

--- ultralytics/custom/posetracker.py
+++ ultralytics/custom/posetracker.py
@@ -464,7 +464,7 @@ class PoseTracker(BOTSORT):
                 in_gate = bbox_maha_dists[j] < self.box_gate_thresh
                 pose_reliable = pose_disim[j] < 0.25
                 if has_iou or in_gate or pose_reliable:
-                    box_score = min(iou_dists_refined[j], max(bbox_maha_dists[j] / self.box_gate_thresh, 0.1))
+                    box_score = min(iou_dists_refined[j], max(bbox_maha_dists[j] / self.box_gate_thresh, 0.5))
                     # if track.state == TrackState.Tracked:
                     #     dists[i, j] = box_score * self.WO_IOU + pose_disim[j] * self.WO_POSE + reid_matrix[i, j] * self.WO_REID
                     # else:
