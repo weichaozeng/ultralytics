8316064e18d3b15c4030c27bb79e79dc268ea734
weichaozeng <zvc22@outlook.com>
2026-03-05 09:25:26 +0800

    update

--- infer.py
+++ infer.py
@@ -91,7 +91,9 @@ def detect_track(args, model, frames, tracker):
     results = []
     i = 0
     for frame_cv2 in frames:
-            # print(i)
+            print(i)
+            if i in [36, 37]:
+                print("here")
             i += 1
             with torch.no_grad():
                 with autocast():
