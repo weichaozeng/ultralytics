05f6d78bbb2bc057b961066c9969d3eed03494df
weichaozeng <zvc22@outlook.com>
2026-03-05 11:51:30 +0800

    update

--- ultralytics/models/yolo/detect/predict.py
+++ ultralytics/models/yolo/detect/predict.py
@@ -63,7 +63,7 @@ class DetectionPredictor(BasePredictor):
         preds = nms_func(
             preds,
             self.args.conf,
-            self.args.iou,
+            0.45, # self.args.iou,
             self.args.classes,
             True, # self.args.agnostic_nms,
             max_det=self.args.max_det,
